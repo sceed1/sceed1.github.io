9a53c8ff54040d0e32dec9824506ad2e8543fedb
Matthias Kartzke <matthias.kartzke@datev.de>
2022-07-17 18:08:59 +0200

    finished changes

--- src/app/stock-sentiment/stock-sentiment.component.ts
+++ src/app/stock-sentiment/stock-sentiment.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
+import { SentimentData } from '../model/stock';
 import { StockService } from '../stock.service';
 
 @Component({
@@ -11,11 +12,18 @@ import { StockService } from '../stock.service';
 export class StockSentimentComponent {
 
   companyName$: Observable<string>;
+  sentimentData$: Observable<SentimentData[]>;
   stockId: string;
 
   constructor(private readonly stockService: StockService, readonly ars: ActivatedRoute) {
     this.stockId = ars.snapshot.paramMap.get('stockId')!;
     this.companyName$ = this.stockService.retrieveCompanyName(this.stockId);
+
+    const date = new Date();
+    const startDate = new Date(date.getFullYear(), date.getMonth() - 3, 1).toLocaleDateString('en-CA');
+    const endDate = new Date(date.getFullYear(), date.getMonth(), 0).toLocaleDateString('en-CA');
+
+    this.sentimentData$ = this.stockService.retrieveSentimentData(this.stockId, startDate, endDate);
   }
 
 }
